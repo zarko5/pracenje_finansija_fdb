a34196114c8947eb16248b82271b08cdb03e13ef
zarko5 <61950179+zarko5@users.noreply.github.com>
2026-06-29 01:39:11 +0200

    foreign key and some db manager changes

--- db/database_manager.py
+++ db/database_manager.py
@@ -1,50 +1,58 @@
 import sqlite3
 
 class DatabaseManager:
-
     def __init__(self, db_path="finance.db"):
         self.db_path = db_path
 
     def connect(self):
-        return sqlite3.connect(self.db_path)
+        conn = sqlite3.connect(self.db_path)
+        conn.execute("PRAGMA foreign_keys = ON;")
+        return conn
     
     def execute(self, query, params=()):
-        conn = self.connect()
-        cursor = conn.cursor()
-        cursor.execute(query, params)
-        conn.commit()
-        conn.close()
+        with self.connect() as conn:
+            cursor = conn.cursor()
+            cursor.execute(query, params)
+            conn.commit()
+
+    def fetch_one(self, query, params=()):
+        with self.connect() as conn:
+            cursor = conn.cursor()
+            cursor.execute(query, params)
+            return cursor.fetchone()
+
+    def fetch_all(self, query, params=()):
+        with self.connect() as conn:
+            cursor = conn.cursor()
+            cursor.execute(query, params)
+            return cursor.fetchall()
 
     def initialize_database(self):
         self.execute("""
-         CREATE TABLE IF NOT EXISTS categories (
+        CREATE TABLE IF NOT EXISTS categories (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL UNIQUE)
-            """)
+            name TEXT NOT NULL UNIQUE
+        );
+        """)
         
         self.execute("""
         CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             username TEXT NOT NULL UNIQUE,
             password_hash TEXT NOT NULL
-            );
+        );
         """)
         
         self.execute("""
         CREATE TABLE IF NOT EXISTS transactions (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-
             user_id INTEGER NOT NULL,
             category_id INTEGER NOT NULL,
-
             amount REAL NOT NULL,
-            transaction_type TEXT NOT NULL,
-            transaction_date TEXT NOT NULL,
-
+            transaction_type TEXT NOT NULL, -- 'prihod' ili 'trošak'
+            transaction_date TEXT NOT NULL, -- YYYY-MM-DD format
             description TEXT,
-
-            FOREIGN KEY (user_id) REFERENCES users(id),
+            FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
             FOREIGN KEY (category_id) REFERENCES categories(id)
-            );
+        );
         """)
-        
--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-import sqlite3
-
 from db.database_manager import DatabaseManager
 
 def main():
-
-    # initialize_database()
     db = DatabaseManager()
-    conn = db.connect()
     db.initialize_database()
-    conn.close()
+    print("Baza inicijalizovana.")
     print("Application started")
 
 if __name__ == "__main__":
