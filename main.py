b5f9fae69b8ee442ffaae45729b850f96642733c
zarko <61950179+zarko5@users.noreply.github.com>
2026-08-27 14:41:34 +0200

    export models and quick test

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 from db.database_manager import DatabaseManager
+import models as md
+from argon2 import PasswordHasher
+# from models import *
 
 def main():
     db = DatabaseManager()
     db.initialize_database()
-    print("Baza inicijalizovana.")
-    print("Application started")
+    print("Baza inicijalizovana")
+
+    # test modela
+    category = md.Category(1, "gorivo")
+    print(category)
+
+    transakcija = md.Transaction(1, 1, 50,"prihodi","2026-02-03","plata")
+    print(transakcija)
+
+    # test user modela
+    ph = PasswordHasher()
+    user = md.User(1, "miki", ph.hash("password"))   
+    print(user)
+    print(user.password_hash) 
+
 
 if __name__ == "__main__":
     main()
---
+++ models/__init__.py
@@ -0,0 +1,3 @@
+from .category import Category
+from .transaction import Transaction
+from .user import User
