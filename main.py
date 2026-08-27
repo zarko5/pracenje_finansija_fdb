86279a2aa038122669924a57c9fe93c8b5613f3b
zarko <61950179+zarko5@users.noreply.github.com>
2026-08-27 15:55:29 +0200

    register user, dump

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from db.database_manager import DatabaseManager
 import models as md
-from argon2 import PasswordHasher
+
 # from models import *
 
 def main():
     db = DatabaseManager()
     db.initialize_database()
     print("Baza inicijalizovana")
+    test_models()
 
+
+
+def test_models():
     # test modela
     category = md.Category(1, "gorivo")
     print(category)
@@ -15,11 +19,6 @@ def main():
     transakcija = md.Transaction(1, 1, 50,"prihodi","2026-02-03","plata")
     print(transakcija)
 
-    # test user modela
-    ph = PasswordHasher()
-    user = md.User(1, "miki", ph.hash("password"))   
-    print(user)
-    print(user.password_hash) 
 
 
 if __name__ == "__main__":
---
+++ services/auth.py
@@ -0,0 +1,20 @@
+from argon2 import PasswordHasher
+
+ph = PasswordHasher()
+
+class AuthService:
+    def __init__(self, db_manager):
+        self.db_manager = db_manager
+
+    # def register_user(self, username, password):
+    def registeruser(self, username, password):
+        user_exists = self.db_manager.fetch_one("SELECT * FROM users WHERE username = ?", (username,))
+        if user_exists:
+            raise ValueError("Korisničko ime već postoji.")
+            # return False
+        pass_hash = ph.hash(password)
+        try:
+            self.db_manager.execute("INSERT INTO users (username, password_hash) VALUES (?, ?)", (username, pass_hash))
+        except Exception as e:
+            print(f"Greška prilikom registracije korisnika: {e}")
+            raise ValueError("Greška prilikom registracije korisnika.")
