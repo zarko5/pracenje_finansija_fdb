676af841e485a44e5db49add001a4cfb5e8ca435
zarko5 <61950179+zarko5@users.noreply.github.com>
2026-08-28 01:04:00 +0200

    auth serv test

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 from db.database_manager import DatabaseManager
 import models as md
-
+import services
 # from models import *
 
 def main():
     db = DatabaseManager()
     db.initialize_database()
     print("Baza inicijalizovana")
-    test_models()
+    test_models(db)
 
 
 
-def test_models():
+def test_models(db):
     # test modela
     category = md.Category(1, "gorivo")
     print(category)
 
     transakcija = md.Transaction(1, 1, 50,"prihodi","2026-02-03","plata")
     print(transakcija)
+
+    athserv = services.AuthService(db)
+    athserv.register_user("pera_username", "password")
+    korisnik = md.User(1,"pera_username","passtest")
+    print(korisnik)
 
 
 
