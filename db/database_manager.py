d4847e47711651f326d20a0fde069fbb097d48ff
zarko <61950179+zarko5@users.noreply.github.com>
2026-08-28 15:11:43 +0200

    delete user, and auth user initial

--- db/database_manager.py
+++ db/database_manager.py
@@ -6,6 +6,7 @@ class DatabaseManager:
 
     def connect(self):
         conn = sqlite3.connect(self.db_path)
+        conn.row_factory = sqlite3.Row
         conn.execute("PRAGMA foreign_keys = ON;")
         return conn
     
--- services/auth.py
+++ services/auth.py
@@ -1,4 +1,6 @@
 from argon2 import PasswordHasher
+from models import User
+
 
 ph = PasswordHasher()
 
@@ -7,7 +9,7 @@ class AuthService:
         self.db_manager = db_manager
 
     # def register_user(self, username, password):
-    def register_user(self, username: str, password: str):
+    def register_user(self, username: str, password: str) -> bool:
         user_exists = self.db_manager.fetch_one("SELECT * FROM users WHERE username = ?", (username,))
         if user_exists:
             raise ValueError("Korisničko ime već postoji.")
@@ -18,3 +20,24 @@ class AuthService:
         except Exception as e:
             print(f"Greška prilikom registracije korisnika: {e}")
             raise ValueError("Greška prilikom registracije korisnika.")
+        return True
+
+    def delete_user(self, username: str) -> bool:
+        user_exists = self.db_manager.fetch_one("SELECT * FROM users WHERE username = ?", (username,))
+        if user_exists:
+            self.db_manager.execute("DELETE FROM users WHERE username = ?", (username,))
+            return True
+        raise ValueError("Korisnik ne postoji.")
+
+    def authenticate_user(self, username: str, password: str) -> bool:
+        user_row = self.db_manager.fetch_one("SELECT * FROM users WHERE username = ?", (username,))
+        if user_row is None:
+            return None
+
+        try:
+            ph.verify(user_row["password_hash"],password)
+            return User(user_row["id"],user_row["username"],user_row["password_hash"])
+        except Exception as e:
+            print("Greska u autentifikaciji")
+            return None
+
