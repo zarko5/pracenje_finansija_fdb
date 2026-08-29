b9e2c5a9aa027c1cb36ddbddce2a959f3a2f7ff8
zarko5 <61950179+zarko5@users.noreply.github.com>
2026-08-29 14:00:42 +0200

    ideje za finserv

--- services/finance_service.py
+++ services/finance_service.py
@@ -4,6 +4,9 @@ class FinanceService():
     def __init__(self,db_manager):
         self.db_manager = db_manager
 
+    # trebalo bi nekoliko izmena ovde, transakcije bi trebalo da se prate po korisniku
+    # korisnik takodje treba da bude autentifikovan, 
+    # i ili da se validiraju podatci, u smislu klasa itd za kategoreje recimo
     def add_transaction(self, transaction: Transaction) -> bool:
         try:
             self.db_manager.execute("INSERT INTO transactions (category_id, amount, type, date, description) VALUES (?, ?, ?, ?, ?)", 
@@ -12,3 +15,7 @@ class FinanceService():
         except Exception as e:
             print(f"greska prilikom dodavanja transakcije: {e}")
             return False
+
+    ## bilo bi dobro vrv isto kao sto je u auth servisu
+    # da imamo svaku operaciju, apdejt, delete, i get
+    
