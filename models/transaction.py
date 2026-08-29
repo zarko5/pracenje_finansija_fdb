3239fdaee260d172476d73b615df488121548dea
zarko5 <61950179+zarko5@users.noreply.github.com>
2026-08-29 14:05:21 +0200

    dodavanje usera na transakciju

--- models/transaction.py
+++ models/transaction.py
@@ -1,11 +1,12 @@
 class Transaction:
     # mozda izmenjati kasnije za transaction date da bude zap tip datum
-    def __init__(self, transaction_id: int, category_id: int, amount: float ,transaction_type: str, transaction_date: str ,desc: str):
+    def __init__(self, transaction_id: int, user_id: int, category_id: int, amount: float ,transaction_type: str, transaction_date: str ,desc: str):
         self.id = transaction_id
+        self.user_id = user_id
         self.category_id = category_id
         self.amount = amount
         self.type = transaction_type
         self.date = transaction_date
         self.description = desc
     def __str__(self) -> str:
-        return f"Transaction(id={self.id}, category_id={self.category_id}, amount={self.amount}, type='{self.type}', date='{self.date}', description='{self.description}')"
+        return f"Transaction(id={self.id}, user={self.user_id}, category_id={self.category_id}, amount={self.amount}, type='{self.type}', date='{self.date}', description='{self.description}')"
--- services/finance_service.py
+++ services/finance_service.py
@@ -4,13 +4,12 @@ class FinanceService():
     def __init__(self,db_manager):
         self.db_manager = db_manager
 
-    # trebalo bi nekoliko izmena ovde, transakcije bi trebalo da se prate po korisniku
-    # korisnik takodje treba da bude autentifikovan, 
+    # korisnik takodje treba da bude autentifikovan
     # i ili da se validiraju podatci, u smislu klasa itd za kategoreje recimo
     def add_transaction(self, transaction: Transaction) -> bool:
         try:
-            self.db_manager.execute("INSERT INTO transactions (category_id, amount, type, date, description) VALUES (?, ?, ?, ?, ?)", 
-                                    (transaction.category_id, transaction.amount, transaction.type, transaction.date, transaction.description))
+            self.db_manager.execute("INSERT INTO transactions (user_id, category_id, amount, type, date, description) VALUES (?, ?, ?, ?, ?)", 
+                                    (transaction.user_id,transaction.category_id, transaction.amount, transaction.type, transaction.date, transaction.description))
             return True
         except Exception as e:
             print(f"greska prilikom dodavanja transakcije: {e}")
@@ -18,4 +17,3 @@ class FinanceService():
 
     ## bilo bi dobro vrv isto kao sto je u auth servisu
     # da imamo svaku operaciju, apdejt, delete, i get
-    
