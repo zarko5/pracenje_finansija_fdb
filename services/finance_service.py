96e48e1e48864e0d67f90fedfaf97cf4b719b73b
zarko5 <61950179+zarko5@users.noreply.github.com>
2026-08-29 17:04:52 +0200

    add some statistics in finance service, total income, expenses, balance

--- services/finance_service.py
+++ services/finance_service.py
@@ -16,7 +16,7 @@ class FinanceService():
             print(f"greska prilikom dodavanja transakcije: {e}")
             return False
 
-    def get_user_transactions(self, user_id: str):   ## za ovo bi mogli mozda da passujemo usera celog, al mnogo je to importa okolo
+    def get_user_transactions(self, user_id: str) -> list[Transaction]:   ## za ovo bi mogli mozda da passujemo usera celog, al mnogo je to importa okolo
         try:
             trans_objects = self.db_manager.fetch_all("SELECT * FROM transactions WHERE user_id = ?",(user_id,))
             lista_transakcija = []
@@ -29,6 +29,26 @@ class FinanceService():
             print(f"greska prilikom trazenja transakcija za korisnika {user_id}")
             return False
 
+    def get_total_income(self, user_id: str) -> float:
+        transakcije_korisnika = self.get_user_transactions(user_id)
+        income = 0
+        ### ovo moze sigurno u one lineru da se resi, al ajde, kasnije
+        for transakcija in transakcije_korisnika:
+            if transakcija.amount > 0: # sve pozitivne kolicine su prihodi
+                income += transakcija.amount
+        return income
+        
+    def get_total_expenses(self, user_id: str) -> float:
+        transakcije_korisnika = self.get_user_transactions(user_id)
+        expenses = 0
+        ### ovo moze sigurno u one lineru da se resi, al ajde, kasnije
+        for transakcija in transakcije_korisnika:
+            if transakcija.amount < 0: # sve pozitivne kolicine su prihodi
+                expenses += transakcija.amount
+        return expenses
+
+    def get_total_balance(self, user_id: str) -> float:
+        return self.get_total_income - self.get_total_expenses
 
 
     ## bilo bi dobro vrv isto kao sto je u auth servisu
