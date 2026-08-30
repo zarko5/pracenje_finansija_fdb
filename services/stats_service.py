5a52b69e085d2b15e2bdbae8d1fcad7e11bf8efa
zarko5 <61950179+zarko5@users.noreply.github.com>
2026-08-30 15:33:24 +0200

    stats service proto

---
+++ services/stats_service.py
@@ -0,0 +1,11 @@
+from models import *
+
+class StatsService:
+    def __init__(self,db_manager):
+        self.db_manager = db_manager
+
+    # def get_total_income(self, user):
+        ### sad ovde bi bila fora da zovemo 
+
+### ovo cu da pauziram trenutno, jer ce pozivati fora raznorazne funkcije od finance service, 
+### a glupo mi da kaskadiram ovde objekat i za to
